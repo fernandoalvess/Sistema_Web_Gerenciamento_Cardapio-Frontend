af494988a9cbb7627f4f6ca24fd6814d8b22144e
fernandoalvess <fernandoumalves@gmail.com>
2025-08-16 16:08:57 -0300

    feat: ícone para mostrar/esconder a senha

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,4 +1,4 @@
-// Aguarda o conteúdo da página carregar completamente
+// Tela de carregamento
 window.addEventListener('load', function() {
     const loader = document.getElementById('loader');
     const mainContent = document.getElementById('main-content');
@@ -19,4 +19,19 @@ window.addEventListener('load', function() {
     }
 
     typeWriter(textToType, 0);
+});
+
+// Mostrar/Esconder a senha
+const passwordInput = document.getElementById('password');
+const togglePasswordIcon = document.getElementById('toggle-password');
+
+togglePasswordIcon.addEventListener('click', function() {
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+
+    if (type === 'password') {
+        this.src = 'assets/images/icons/eye-off.svg';
+    } else {
+        this.src = 'assets/images/icons/eye-on.svg';
+    }
 });
