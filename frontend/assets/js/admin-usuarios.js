d71eb63fd64c7ec17242c19fd188879a691e7f79
fernandoalvess <fernandoumalves@gmail.com>
2025-08-26 08:47:05 -0300

    feat: modificando o modal de cadastrar usuário

--- frontend/assets/js/admin-usuarios.js
+++ frontend/assets/js/admin-usuarios.js
@@ -3,8 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const addUserForm = document.getElementById('add-user-form');
     const deleteUserModal = document.getElementById('delete-user-modal');
     const confirmDeleteBtn = document.querySelector('#delete-user-modal .button-danger');
+    const phoneInput = document.getElementById('telefone');
     
-    // NOVO: Guarda a URL original da página 
+    //Guarda a URL original da página 
     const originalUrl = window.location.pathname;
 
     // Ação de cadastrar usuário (simulação)
@@ -19,6 +20,37 @@ document.addEventListener('DOMContentLoaded', function() {
 
             // Muda a URL para a original
             history.pushState({}, '', originalUrl);
+        });
+    }
+
+    if (phoneInput) {
+
+        const maskOptions = {
+            mask: '(00) 00000-0000'
+        };
+        const phoneMask = IMask(phoneInput, maskOptions);
+    }
+
+    // O NÚMERO LIMPO PARA O BACKEND
+    const form = document.querySelector('form');
+    
+    if (form) {
+        form.addEventListener('submit', function(event) {
+            // Pega a instância da máscara 
+            const phoneMask = IMask.instances.find(mask => mask.el === phoneInput);
+
+            if (phoneMask) {
+                // Pega apenas os números, sem a formatação
+                const unmaskedValue = phoneMask.unmaskedValue;
+
+                // Cria um campo de input escondido para enviar o número limpo
+                const hiddenInput = document.createElement('input');
+                hiddenInput.type = 'hidden';
+                hiddenInput.name = 'telefone_limpo'; //oq o backend vai receber
+                hiddenInput.value = unmaskedValue;
+
+                form.appendChild(hiddenInput);
+            }
         });
     }
 
